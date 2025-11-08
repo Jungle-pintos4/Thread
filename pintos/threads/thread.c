10da8b01f9a788bb685a56028c2afe2371d1faff
dongggggggg <dlehdrb020@gmail.com>
2025-11-08 11:45:36 +0900

    [이동규] priority schedule 구현
    1. 기본적인 priority schedule은 우선순위를 볼 수 있게 해야함
    2. 지금은 sort없이 insert -> list_insert_ordered로 할 수 있도록 변경

--- pintos/threads/thread.c
+++ pintos/threads/thread.c
@@ -67,6 +67,7 @@ static void schedule (void);
 static tid_t allocate_tid (void);
 
 bool wakeup_tick_less(const struct list_elem *a, const struct list_elem *b, void *aux);
+bool priority_less(const struct list_elem *a, const struct list_elem *b, void *aux);
 
 /* T가 유효한 스레드를 가리키는 것으로 보이면 true를 반환. */
 #define is_thread(t) ((t) != NULL && (t)->magic == THREAD_MAGIC)
@@ -206,7 +207,9 @@ thread_create (const char *name, int priority,
 
 	/* 실행 큐에 추가. */
 	thread_unblock (t);
-
+	if (t->priority > thread_current()->priority){
+		thread_yield();
+	}
 	return tid;
 }
 
@@ -238,9 +241,18 @@ thread_unblock (struct thread *t) {
 
 	old_level = intr_disable ();
 	ASSERT (t->status == THREAD_BLOCKED);
-	list_push_back (&ready_list, &t->elem);
+	// list_push_back (&ready_list, &t->elem);
+	list_insert_ordered(&ready_list,  &t->elem, priority_less, NULL);
 	t->status = THREAD_READY;
 	intr_set_level (old_level);
+}
+
+// 비교 함수
+bool priority_less(const struct list_elem *a, const struct list_elem *b, void *aux)
+{
+	struct thread *ta = list_entry(a, struct thread, elem);
+	struct thread *tb = list_entry(b, struct thread, elem);
+	return ta->priority > tb->priority;
 }
 
 
@@ -303,7 +315,8 @@ thread_yield (void) {
 
 	old_level = intr_disable ();
 	if (curr != idle_thread)
-		list_push_back (&ready_list, &curr->elem);
+		// list_push_back (&ready_list, &curr->elem);
+		list_insert_ordered(&ready_list,  &curr->elem, priority_less, NULL);
 	do_schedule (THREAD_READY);
 	intr_set_level (old_level);
 }
@@ -395,6 +408,7 @@ void thread_awake(int64_t wakeup_tick){
 void
 thread_set_priority (int new_priority) {
 	thread_current ()->priority = new_priority;
+	thread_yield();
 }
 
 /* 현재 스레드의 우선순위를 반환. */
